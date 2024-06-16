290697542a735f464c354da5da1e58c804708cf2
Aira Jena <airajena.hack@gmail.com>
2024-06-16 19:31:37 +0530

    feat: Integrate Redis pub/sub with Socket.io for message broadcasting

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,16 +1,37 @@
 import { Server } from "socket.io";
+require('dotenv').config();
+
+import Redis from "ioredis";
+
+const pub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 22286,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
+
+const sub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 22286,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
+
+
+
 
 class SocketService {
   private _io: Server;
 
   constructor() {
-    console.log("Init SocketService")
+    console.log("Init Socket Service...");
     this._io = new Server({
-        cors: {
-            allowedHeaders: ["*"],
-            origin: "*",
-        },
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
     });
+    sub.subscribe("MESSAGES");
   }
 
   public initListeners() {
@@ -21,7 +42,15 @@ class SocketService {
       console.log(`New Socket Connected`, socket.id);
       socket.on("event:message", async ({ message }: { message: string }) => {
         console.log("New Message Rec.", message);
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
       });
+    });
+
+    sub.on("message", async (channel, message) => {
+      if (channel === "MESSAGES") {
+        console.log("new message from redis", message);
+        io.emit("message", message);
+      }
     });
   }
   get io(){
